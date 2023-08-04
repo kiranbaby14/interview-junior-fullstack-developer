5222ce5cf1cd9018b35470d8370fd298aa744195
kiranbaby14 <kiranbaby256@gmail.com>
2023-08-04 16:54:31 +0100

    api calls update

--- interview-backend/src/api/api.service.ts
+++ interview-backend/src/api/api.service.ts
@@ -7,7 +7,7 @@ export class ApiService {
     private readonly apiUrl: string;
 
     constructor() {
-      this.apiUrl = process.env.DB_PROXY;
+        this.apiUrl = process.env.DB_PROXY;
     }
 
     async getAllCityDetails(): Promise<string[]> {
@@ -15,18 +15,22 @@ export class ApiService {
             const response = await axios.get(this.apiUrl);
             const cityDetails = response.data.map(({ cityName, count }) => ({ cityName, count }));
             return cityDetails;
-          } catch (error) {
+        } catch (error) {
             return error.message;
-          }
+        }
     }
 
-    async getCityNameDetails(cityName: String):  Promise<string[]>  {
-        const response = await axios.get(this.apiUrl);
-        const matchingCityNames = response.data
-          .filter((city) => city.cityName.toLowerCase().includes(cityName.toLowerCase()))
-          .map(({ cityName, count }) => ({ cityName, count }));
-    
-        return matchingCityNames;
+    async getCityNameDetails(cityName: String): Promise<string[]> {
+        try {
+            const response = await axios.get(this.apiUrl);
+            const matchingCityNames = response.data
+                .filter((city) => city.cityName.toLowerCase().includes(cityName.toLowerCase()))
+                .map(({ cityName, count }) => ({ cityName, count }));
+
+            return matchingCityNames;
+        } catch (error) {
+            return error.message;
+        }
     }
 
 }
