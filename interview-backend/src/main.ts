4ec529388699d34c729a3a6de6dcb5755baa10ae
kiranbaby14 <kiranbaby256@gmail.com>
2023-08-05 23:25:52 +0100

    add CORS to backend

--- interview-backend/src/main.ts
+++ interview-backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    // Enable CORS for all domains
+    app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
