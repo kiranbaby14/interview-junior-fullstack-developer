7dd1a11950e29e95c739862505ab9b2fdf86d9fa
kiranbaby14 <kiranbaby256@gmail.com>
2023-08-05 23:13:14 +0100

    Implement basic api call functionality

---
+++ interview-frontend/src/app/pages/home/home.component.ts
@@ -0,0 +1,35 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit{
+
+  public getJsonValue: any;
+  public postJsonValue: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getMethod();
+    this.postMethod();
+  }
+
+  public getMethod(){
+    this.http.get("http://localhost:8000/items").subscribe((data) => {
+      this.getJsonValue = data;
+    });
+  }
+
+  public postMethod(){
+    
+    let body = {}
+
+    this.http.post("http://localhost:8000/items", body).subscribe((data) => {
+      this.postJsonValue = data;
+    });
+  }
+}
