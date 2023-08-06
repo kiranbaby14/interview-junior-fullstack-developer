c9de05082780a9a626272be080d8b94aae856f34
kiranbaby14 <kiranbaby256@gmail.com>
2023-08-06 15:03:43 +0100

    backend updates

--- interview-backend/src/app.module.ts
+++ interview-backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
-import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
   imports: [ApiModule,
